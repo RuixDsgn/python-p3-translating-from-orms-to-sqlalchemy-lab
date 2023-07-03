f705bacaba5a8248bc1c35e431d887626f634400
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-07-03 12:53:03 -0700

    completed lab with all tests passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,37 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    joey = Dog(
+        name="joey",
+        breed="cocker spaniel"
+    )
+
+    session.add(joey)
+    session.commit()
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    return dogs
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog).filter(Dog.name == name).first()
+
+    return query
+
 
 def find_by_id(session, id):
-    pass
+    dog_id = session.query(Dog).filter(Dog.id == id).first()
+
+    return dog_id
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed=breed
+    session.add(dog)
+    session.commit()
